33e5f19eee3a7b33d05e10af4e427a72b42a0873
dd-max <max_mars@bk.ru>
2016-10-06 21:31:16 +1000

    06.10

--- templates/page--design_page.tpl.php
+++ templates/page--design_page.tpl.php
@@ -73,94 +73,98 @@
  * @ingroup templates
  */
 ?>
-<header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
+<header id="navbar" role="banner">
   <div class="<?php print $container_class; ?>">
-    <div class="navbar-header">
-      <?php if ($logo): ?>
-        <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-        </a>
-      <?php endif; ?>
+    <div class="row">
+      <div class="navbar-header">
+        <div id="logo-wrapper" class="col-xs-4 col-sm-2">
+          <?php if ($logo): ?>
+            <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+              <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+            </a>
+          <?php endif; ?>
 
-      <?php if (!empty($site_name)): ?>
-        <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>
+          <?php if (!empty($site_name)): ?>
+            <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
+          <?php endif; ?>
+        </div>
 
-      <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-          <span class="sr-only"><?php print t('Toggle navigation'); ?></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-      <?php endif; ?>
+        <?php if (!empty($page['header'])): ?>
+          <?php print render($page['header']); ?>
+        <?php endif; ?>
+      </div>
     </div>
+  </div>
+  <div id="main-menu-wrapper">
 
     <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      <div class="navbar-collapse collapse">
-        <nav role="navigation">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
-        </nav>
+      <div class="container">
+        <div class="row">
+          <div class="button-container visible-xs visible-sm">
+            <div class="btn-wrapper visible-xs visible-sm pull-left">
+              <button type="button" class="navbar-toggle visible-xs visible-sm" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only"><?php print t('Toggle navigation'); ?></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+            </div>
+            <p><?php print t('Site menu'); ?></p>
+          </div>
+
+          <div class="navbar-collapse collapse hidden-xs hidden-sm">
+            <nav role="navigation">
+              <?php if (!empty($primary_nav)): ?>
+                <?php print render($primary_nav); ?>
+              <?php endif; ?>
+              <?php if (!empty($secondary_nav)): ?>
+                <?php print render($secondary_nav); ?>
+              <?php endif; ?>
+              <?php if (!empty($page['navigation'])): ?>
+                <?php print render($page['navigation']); ?>
+              <?php endif; ?>
+            </nav>
+          </div>
+        </div>
       </div>
+
     <?php endif; ?>
   </div>
 </header>
 
-<div class="main-container">
+<div class="main-container container-fluid">
 
-  <header role="banner" id="page-header">
-    <?php if (!empty($site_slogan)): ?>
-      <p class="lead"><?php print $site_slogan; ?></p>
+  <!-- <header role="banner" id="page-header">
+</header> -->
+
+<div class="row">
+
+  <div class="col-xs-12 h1-bread-wrapper">
+    <?php print render($title_prefix); ?>
+    <?php if (!empty($title)): ?>
+      <h1 class="page-header"><?php print $title; ?></h1>
     <?php endif; ?>
-
-    <?php print render($page['header']); ?>
-  </header> <!-- /#page-header -->
-
-  <div class="row">
-
-    <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_first']); ?>
-      </aside>  <!-- /#sidebar-first -->
+    <?php print render($title_suffix); ?>
+    <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+    <?php print $messages; ?>
+    <?php if (!empty($tabs)): ?>
+      <?php print render($tabs); ?>
     <?php endif; ?>
+  </div>
 
-    <section<?php print $content_column_class; ?>>
-      <?php if (!empty($page['highlighted'])): ?>
-        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
-      <?php endif; ?>
-      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php if (!empty($tabs)): ?>
-        <?php print render($tabs); ?>
-      <?php endif; ?>
-      <?php if (!empty($page['help'])): ?>
-        <?php print render($page['help']); ?>
-      <?php endif; ?>
-      <?php if (!empty($action_links)): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-    </section>
-
-    <?php if (!empty($page['sidebar_second'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_second']); ?>
-      </aside>  <!-- /#sidebar-second -->
+  <section>
+    <?php if (!empty($page['highlighted'])): ?>
+      <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
     <?php endif; ?>
+    <a id="main-content"></a>
+    <?php if (!empty($page['help'])): ?>
+      <?php print render($page['help']); ?>
+    <?php endif; ?>
+    <?php if (!empty($action_links)): ?>
+      <ul class="action-links"><?php print render($action_links); ?></ul>
+    <?php endif; ?>
+    <?php print render($page['content']); ?>
+  </section>
 
   </div>
 </div>
--- templates/page--product.tpl.php
+++ templates/page--product.tpl.php
@@ -135,52 +135,52 @@
 <div class="main-container container-fluid">
 
   <!-- <header role="banner" id="page-header">
-  </header> -->
+</header> -->
 
-  <div class="row">
+<div class="row">
 
-    <div class="col-xs-12 h1-bread-wrapper">
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
-      <?php print $messages; ?>
-      <?php if (!empty($tabs)): ?>
-        <?php print render($tabs); ?>
-      <?php endif; ?>
-    </div>
-
-    <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-xs-12 col-sm-12 col-md-3 sidebar-first-wrapper" role="complementary">
-        <div class="row">
-          <?php print render($page['sidebar_first']); ?>
-        </div>
-      </aside>  <!-- /#sidebar-first -->
+  <div class="col-xs-12 h1-bread-wrapper">
+    <?php print render($title_prefix); ?>
+    <?php if (!empty($title)): ?>
+      <h1 class="page-header"><?php print $title; ?></h1>
     <?php endif; ?>
-
-    <section<?php print $content_column_class; ?>>
-      <?php if (!empty($page['highlighted'])): ?>
-        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
-      <?php endif; ?>
-      <a id="main-content"></a>
-      <?php if (!empty($page['help'])): ?>
-        <?php print render($page['help']); ?>
-      <?php endif; ?>
-      <?php if (!empty($action_links)): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-    </section>
-
-    <?php if (!empty($page['sidebar_second'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_second']); ?>
-      </aside>  <!-- /#sidebar-second -->
+    <?php print render($title_suffix); ?>
+    <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+    <?php print $messages; ?>
+    <?php if (!empty($tabs)): ?>
+      <?php print render($tabs); ?>
     <?php endif; ?>
-
   </div>
+
+  <?php if (!empty($page['sidebar_first'])): ?>
+    <aside class="col-xs-12 col-sm-12 col-md-3 sidebar-first-wrapper" role="complementary">
+      <div class="row">
+        <?php print render($page['sidebar_first']); ?>
+      </div>
+    </aside>  <!-- /#sidebar-first -->
+  <?php endif; ?>
+
+  <section<?php print $content_column_class; ?>>
+  <?php if (!empty($page['highlighted'])): ?>
+    <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+  <?php endif; ?>
+  <a id="main-content"></a>
+  <?php if (!empty($page['help'])): ?>
+    <?php print render($page['help']); ?>
+  <?php endif; ?>
+  <?php if (!empty($action_links)): ?>
+    <ul class="action-links"><?php print render($action_links); ?></ul>
+  <?php endif; ?>
+  <?php print render($page['content']); ?>
+</section>
+
+<?php if (!empty($page['sidebar_second'])): ?>
+  <aside class="col-sm-3" role="complementary">
+    <?php print render($page['sidebar_second']); ?>
+  </aside>  <!-- /#sidebar-second -->
+<?php endif; ?>
+
+</div>
 </div>
 
 <?php if (!empty($page['footer'])): ?>
